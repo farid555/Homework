0f297870af52178b66f6f11550a253e90e871a98
farid555 <mohammad.farid@centria.fi>
2020-01-22 23:48:28 +0200

    exercise_13 done

--- part1/variables/exercise_13/Program.cs
+++ part1/variables/exercise_13/Program.cs
@@ -7,6 +7,10 @@ namespace exercise_13
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give a number !");
+      string user_Input = Console.ReadLine();
+      double double_Value = Convert.ToDouble(user_Input);
+      Console.WriteLine("You gave: " + double_Value);
 
     }
   }
