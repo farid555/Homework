79cb0bf5cc2699fa6d21aaf3d1d451490692d019
farid555 <mohammad.farid@centria.fi>
2020-01-24 11:17:26 +0200

    exercise_23 done

--- part1/conditionals/exercise_24/Program.cs
+++ part1/conditionals/exercise_24/Program.cs
@@ -8,6 +8,19 @@ namespace exercise_24
     {
 
       // Write your code here:
+      int speed = 120;
+      Console.WriteLine ("Your Speed: ");
+      string input_speed = Console.ReadLine();
+
+      int input_value = Convert.ToInt32(input_speed);
+
+      
+
+      if( input_value> speed)
+      {
+          Console.WriteLine("speeding! \n");
+      }
+      
       
     }
   }
--- part1/conditionals/exercise_25/Program.cs
+++ part1/conditionals/exercise_25/Program.cs
@@ -7,7 +7,24 @@ namespace exercise_25
     public static void Main(string[] args)
     {
       // Write your code here:
+     
+      int year = 1984; 
 
+      Console.WriteLine ("Give a number!");
+      string user_input = Console.ReadLine();
+
+      int input_value = Convert.ToInt32(user_input);
+
+      if(input_value == year)
+          {
+              Console.WriteLine ("Orwell");
+          }
+
+          else
+
+          {
+              Console.WriteLine(user_input);
+          }
     }
   }
 }
