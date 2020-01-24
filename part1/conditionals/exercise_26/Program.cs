721173e127c44f888b5f2a5de0949cd6e4050517
farid555 <mohammad.farid@centria.fi>
2020-01-24 12:42:12 +0200

    exercise_26 done

--- part1/conditionals/exercise_26/Program.cs
+++ part1/conditionals/exercise_26/Program.cs
@@ -5,8 +5,22 @@ namespace exercise_26
   class Program
   {
     public static void Main(string[] args)
-    {
+    {  
+
+      int year = 1900;
       // Write your code here:
+      Console.WriteLine ("Give your age:");
+      string user_age = Console.ReadLine();
+
+      int value = Convert.ToInt32(user_age);
+      
+      if (value <year)
+      {
+          Console.WriteLine ("You are old:" );
+      }
+
+
+      
 
     }
   }
