51abf169bc82aa1840cc544535214e12099b6fab
farid555 <mohammad.farid@centria.fi>
2020-01-24 13:27:19 +0200

    execrise_27 done

--- part1/conditionals/exercise_27/Program.cs
+++ part1/conditionals/exercise_27/Program.cs
@@ -7,7 +7,19 @@ namespace exercise_27
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give a number:");
+      string input_number = Console.ReadLine();
 
+      int value = Convert.ToInt32(input_number);
+      
+      if (value> 0)
+      {
+        Console.WriteLine("It is positive.");
+      }
+      else
+      {
+        Console.WriteLine("It is not positive.");
+      }
     }
   }
 }
