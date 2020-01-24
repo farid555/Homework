74f05bdc8db7c95ca73a0345212422bfe4b4b423
farid555 <mohammad.farid@centria.fi>
2020-01-24 09:47:56 +0200

    exercise_17 done

--- part1/calculations/exercise_21/Program.cs
+++ part1/calculations/exercise_21/Program.cs
@@ -6,8 +6,21 @@ namespace exercise_21
   {
     public static void Main(string[] args)
     {
-
+              
       // Write your code here:
+     Console.WriteLine ("Give the first number!");
+     string input_one = Console.ReadLine();
+
+     Console.WriteLine  ("Give the second number!");
+     string input_two = Console.ReadLine();
+
+     int value_one = Convert.ToInt32(input_one);
+     int value_two = Convert.ToInt32(input_two);
+
+     double average = (value_one + value_two) / 2.0;
+
+     Console.WriteLine ("The average is " + average);
+
 
     }
   }
