691ecf7cbd859e52649cb263105ace239968e1ac
farid555 <mohammad.farid@centria.fi>
2020-01-25 20:06:00 +0200

    exercise_28 done

--- part1/conditionals/exercise_28/Program.cs
+++ part1/conditionals/exercise_28/Program.cs
@@ -7,6 +7,19 @@ namespace exercise_28
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine ("How old are you?");
+      string user_old = Console.ReadLine();
+
+      int value = Convert.ToInt32(user_old);
+         
+      if(value >= 18)
+      {
+        Console.WriteLine ("You are an adult!");
+      }
+      else
+      {
+        Console.WriteLine ("You are under age!");
+      }
 
     }
   }
