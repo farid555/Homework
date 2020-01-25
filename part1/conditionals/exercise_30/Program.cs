393b53da704d1073155aa0b42e8a6db29056e00e
farid555 <mohammad.farid@centria.fi>
2020-01-25 22:16:31 +0200

    exercise_30 done

--- part1/conditionals/exercise_30/Program.cs
+++ part1/conditionals/exercise_30/Program.cs
@@ -7,7 +7,44 @@ namespace exercise_30
     public static void Main(string[] args)
     {
       // Write your code here:
+    Console.WriteLine ("Give your percent [0 - 100]:");
+    string input_value = Console.ReadLine();
 
+    int user_score = Convert.ToInt32(input_value);
+
+    if (user_score < 0)
+    {
+      Console.WriteLine ("Impossible");
     }
+    else if (user_score <=49 )
+    {
+      Console.WriteLine ("Fail");
+    }
+    else if (user_score <=59)
+    {
+      Console.WriteLine ("Grade: 1");
+    }
+    else if (user_score <=69 )
+    {
+      Console.WriteLine ("Grade: 2");
+    }
+    else if (user_score <=79)
+    {
+      Console.WriteLine ("Grade: 3");
+    }
+    else if (user_score <=89)
+    {
+      Console.WriteLine ("Grade: 4");
+    }
+    else if (user_score <= 100 )
+    {
+      Console.WriteLine ("Grade: 5");
+    }
+    else if (user_score > 100 )
+    {
+      Console.WriteLine ("Outstanding!");
+    }
+    }
+    
   }
 }
