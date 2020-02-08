b845637286abde9c0d7a00214dba8556d70e12eb
farid555 <mohammad.farid@centria.fi>
2020-02-09 01:14:45 +0200

    exercise_71 done

--- part3/lists/exercise_71/Program.cs
+++ part3/lists/exercise_71/Program.cs
@@ -17,7 +17,15 @@ namespace exercise_71
         }
         list.Add(input);
       }
-
+        Console.WriteLine ("Search for?");
+        int user_input = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < list.Count; i++)
+        {
+          if (list[i] == user_input )
+          {
+            Console.WriteLine (user_input + " is at index " + i);
+          }
+        }
     }
   }
 }
