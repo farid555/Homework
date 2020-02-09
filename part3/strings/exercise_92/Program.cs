ba19bc5bafa1e6a728193d14ab6804c5e5a304ec
farid555 <mohammad.farid@centria.fi>
2020-02-09 23:31:24 +0200

    exercise_91 done

--- part3/strings/exercise_92/Program.cs
+++ part3/strings/exercise_92/Program.cs
@@ -7,7 +7,34 @@ namespace exercise_92
   {
     public static void Main(string[] args)
     {
+      // age
+      int age = 9999;
+      //name
+      string name = ("");
+      while(true)
+      {
+        string input = Console.ReadLine();
+        if (input == ""){
+          break;
+        }
+        // split the string in parts[0] (name) and [1] year of birth
+        string[] parts = input.Split(",");
 
+
+        if (age > Convert.ToInt32(parts[1])){
+          age = Convert.ToInt32(parts[1]);
+
+        }
+        // get longest name
+        if (name.Length < parts[0].Length){
+
+          name = parts[0];
+        }
+      }
+         //calculate age of old
+         int old = 2020 - age;
+         Console.WriteLine("Longest name: " + name);
+         Console.WriteLine("Highest age: " + old);
     }
   }
 }
