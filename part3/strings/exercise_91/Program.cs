673808ea25409f7b3385b8d9dc3545790e82b814
farid555 <mohammad.farid@centria.fi>
2020-02-09 23:31:24 +0200

    exercise_91 done

--- part3/strings/exercise_91/Program.cs
+++ part3/strings/exercise_91/Program.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace exercise_91
-{
-  class Program
-  {
-    public static void Main(string[] args)
-    {
+namespace exercise_91 {
+  class Program {
+    public static void Main (string[] args) {
+      int old = 0;
+      string name = "";
 
+      while (true) {
+        string input = Console.ReadLine();
+        if (input == "") {
+          break;
+        }
+        string[] parts = input.Split (",");
+        if (old < Convert.ToInt32(parts[1])) {
+
+            old = Convert.ToInt32 (parts[1]);
+            name = parts[0];
+          }
+        }
+        Console.WriteLine ("Name of the oldest: " + name);
+      }
     }
   }
-}
-
-
-
