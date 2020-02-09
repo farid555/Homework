274197b0a5499b684566193b117e2a6a411d8ce8
farid555 <mohammad.farid@centria.fi>
2020-02-09 05:04:12 +0200

    exercise_77 done

--- part3/lists/exercise_77/Program.cs
+++ part3/lists/exercise_77/Program.cs
@@ -8,9 +8,9 @@ namespace exercise_77
     public static void Main(string[] args)
     {  List<string> strings = new List<string> ();
       // You can try your method here if you want
-      strings.Add ("First");
-      strings.Add ("Second");
-      strings.Add ("Third");
+      strings.Add("First");
+      strings.Add("Second");
+      strings.Add("Third");
                       
        //**remember,this how we print all the items in a list               
       strings.ForEach (Console.WriteLine);
@@ -19,7 +19,8 @@ namespace exercise_77
       RemoveLast (strings);
 
       strings.ForEach (Console.WriteLine);
-    }    //method
+    }    
+         //method
        public static void RemoveLast (List<string> strings)
        {
          // find the last string value
