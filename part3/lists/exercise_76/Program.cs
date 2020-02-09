6e9deb7ed6abbabe694ca591f1a3ae726413d376
farid555 <mohammad.farid@centria.fi>
2020-02-09 04:52:47 +0200

    exercise_76 done

--- part3/lists/exercise_76/Program.cs
+++ part3/lists/exercise_76/Program.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace exercise_76
-{
-  class Program
-  {
-    public static void Main(string[] args)
-    {
+namespace exercise_76 {
+  class Program {
+    public static void Main (string[] args) {
+      List<int> numbers = new List<int> ();
+      numbers.Add (2);
+      numbers.Add (6);
+      numbers.Add (-1);
+      Console.WriteLine (Sum (numbers));
 
+      numbers.Add (5);
+      numbers.Add (1);
+      Console.WriteLine (Sum (numbers));
+    } // method
+
+    public static int Sum (List<int> numbers) {
+      // start from 0
+      int sum = 0;
+
+      for (int i = 0; i < numbers.Count; i++) {
+        int number = numbers[i];
+        sum = sum + number;
+
+      }     //it shows the totals
+           return (sum);
     }
-
   }
 }
-
-
