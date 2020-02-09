1f6feb50d554d2a758fddc42effe6b3a2fdd4be0
farid555 <mohammad.farid@centria.fi>
2020-02-09 02:14:09 +0200

    exercise_72 done

--- part3/lists/exercise_72/Program.cs
+++ part3/lists/exercise_72/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace exercise_72
-{
-  class Program
-  {
-    public static void Main(string[] args)
-    {
+namespace exercise_72 {
+  class Program {
+    public static void Main (string[] args) {
+      List<int> list = new List<int> ();
+      while (true) {
+        int input = Convert.ToInt32 (Console.ReadLine ());
+        if (input == 9999) {
+          break;
+        }
+        list.Add (input);
+      }
+      int smallest = list[0];
+
+      for (int i = 0; i < list.Count; i++) {
+        int number = list[i];
+        if (smallest > number) {
+          smallest = number;
+        }
+      }
+      Console.WriteLine ("Smallest number: " + smallest);
+
+      for (int i = 0; i < list.Count; i++) {
+        if (list[i] == smallest) {
+          Console.WriteLine ("Found at index: " + i);
+        }
+      }
 
     }
   }
