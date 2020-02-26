58a72af44a494ca224f4dc0f2d6d114c1c53f7de
farid555 <mohammad.farid@centria.fi>
2020-02-26 23:18:08 +0200

    exercise_100 done

--- part4/objects/exercise_100/Debt.cs
+++ part4/objects/exercise_100/Debt.cs
@@ -4,19 +4,19 @@ namespace exercise_100
  {
     public class Debt // create class
     {
-        private double blance;
+        private double balance;
         private double interestRate;
 
         public Debt (double initialBalance, double initialInterestRate) //Constructor
         {
-            this.blance = initialBalance;
+            this.balance = initialBalance;
             this.interestRate = initialInterestRate;
         }
         public void PrintBalance () {
-            Console.WriteLine (this.blance); // it'll show me the money
+            Console.WriteLine (this.balance); // it'll show me the money
         }
         public void WaitOneYear () {
-            this.blance = this.blance * interestRate; //debt is increased by multiplying the balance by the interest rate
+            this.balance = this.balance * interestRate; //debt is increased by multiplying the balance by the interest rate
         }
 
     }
