4634aa141bc458edf6e9e9b0f8c52762bd648699
farid555 <mohammad.farid@centria.fi>
2020-02-28 23:12:49 +0200

    exercise_104 done

---
+++ part4/objects/exercise_104/Multiplier.cs
@@ -0,0 +1,16 @@
+using System;
+namespace exercise_104 {
+    public class Multiplier {
+
+        public int number ;
+
+        public Multiplier(int number)
+        {
+          this.number = number;
+        }
+        public int Multiply(int number) 
+        {
+          return this.number = this.number * number;
+        }
+    }
+}
