6ad78d0e451fbe66d8ea217a9a5adbaa5b617669
farid555 <https://github.com/farid555/Homework>
2020-03-01 21:58:37 +0200

    exercise_112 done

--- part4/objectlist/exercise_112/Program.cs
+++ part4/objectlist/exercise_112/Program.cs
@@ -8,7 +8,32 @@ namespace exercise_112
     public static void Main(string[] args)
     {
       // IMPLEMENT YOUR CODE IN HERE!
+    List<TelevisionProgram> list = new List<TelevisionProgram>();
 
+    while(true)
+    {
+      Console.Write("Name: ");
+      String name = Console.ReadLine();
+      if(name == "")
+      {
+        break;
+      } 
+      Console.Write("Duration: ");
+      int duration = Convert.ToInt32(Console.ReadLine());
+
+      list.Add(new TelevisionProgram(name, duration));
+    }
+    Console.WriteLine();
+    Console.Write("Program's maximum duration? ");
+    int max = Convert.ToInt32(Console.ReadLine());
+
+    foreach(TelevisionProgram item in list)
+    {
+      if (item.duration <= max){
+        Console.WriteLine(item);
+
+      }
+    }
 
     }
   }
