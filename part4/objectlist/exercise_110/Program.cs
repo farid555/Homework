6a5e9bdadfd5de8e737a53797739f48021b9102e
farid555 <mohammad.farid@centria.fi>
2020-03-10 22:50:30 +0200

    exercise_110 done

--- part4/objectlist/exercise_110/Program.cs
+++ part4/objectlist/exercise_110/Program.cs
@@ -1,34 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace exercise_110
-{
-  class Program
-  {
-    public static void Main(string[] args)
-    {
+namespace exercise_110 {
+  class Program {
+    public static void Main (string[] args) {
       // IMPLEMENT YOUR CODE IN HERE!
-      List<Item> items = new List<Item>();
-
-      while(true)
-      {
-        Console.Write("Name: ");
-        string name = Console.ReadLine();
-        if(name == "")
+      List<Item> items = new List<Item> ();
+      while (true) {
+        // Console.Write ("Enter a name, empty will stop: ");
+        Console.Write ("Name: ");
+        string name = Console.ReadLine ();
+        if (name == "") 
         {
           break;
         }
-        items.Add(new Item(name));
+        // add name to the list
+        items.Add (new Item (name));
       }
-      Console.WriteLine();
-      foreach (Item name in items)
-      {
-        Console.WriteLine(name);
+      Console.WriteLine ();
+      foreach (Item name in items) {
+        Console.WriteLine (name);
       }
+   
+    }
     }
   }
-}
-
-
-
-
