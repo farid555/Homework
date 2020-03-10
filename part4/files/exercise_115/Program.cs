bedcd174e721337ded2828c514859ebe46e8a301
farid555 <mohammad.farid@centria.fi>
2020-03-11 00:24:19 +0200

    exercise_115 done

--- part4/files/exercise_115/Program.cs
+++ part4/files/exercise_115/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
-namespace exercise_115
-{
-  class Program
-  {
-    public static void Main(string[] args)
-    {
+namespace exercise_115 {
+  class Program {
+    public static void Main (string[] args) {
+         
 
+
+      while (true) {
+        string user_input = Console.ReadLine();
+        if (user_input == "end") {
+          break;
+        }
+         int number = Convert.ToInt32(user_input);
+         Console.WriteLine(number*number*number); 
+
+      }
     }
   }
 }
