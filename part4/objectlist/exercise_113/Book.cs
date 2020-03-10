6995030686e350d955b600ee2f2580968406d743
farid555 <mohammad.farid@centria.fi>
2020-03-10 23:42:00 +0200

    exercise_113 done

--- part4/objectlist/exercise_113/Book.cs
+++ part4/objectlist/exercise_113/Book.cs
@@ -4,18 +4,18 @@ namespace exercise_113
     public class Book
     {
 
-        public string name { get; set; }
+        public string title { get; set; }
         public int pages { get; set; }
         public int year { get; set; }
 
-        public Book(string name, int pages, int year)
+        public Book(string title, int pages, int year)
         {
-            this.name = name;
+            this.title = title;
             this.pages = pages;
             this.year = year;
         }
         public override string ToString(){
-            return this.name + ", " + this.pages + " pages" + ", " + this.year;
+            return this.title + ", " + this.pages + " pages" + ", " + this.year;
         }
     }
 }
