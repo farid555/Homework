d13929787efdeda3579e0c6faed9eb287aeb770e
farid555 <mohammad.farid@centria.fi>
2020-03-10 22:50:30 +0200

    exercise_110 done

--- part4/objectlist/exercise_111/Program.cs
+++ part4/objectlist/exercise_111/Program.cs
@@ -1,41 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace exercise_111
-{
-    class Program
-    {
-        public static void Main(string[] args)
-        {
+namespace exercise_111 {
+    class Program {
+        public static void Main (string[] args) {
             // IMPLEMENT YOUR CODE IN HERE!
             List<PersonalInformation> list = new List<PersonalInformation>();
 
-            while (true)
-            {
-                Console.WriteLine("First name:");
-                string firstName = Console.ReadLine();
-                if (firstName == "")
-                {
+            while (true) {
+                Console.WriteLine ("First name:");
+                string firstName = Console.ReadLine ();
+                if (firstName == "") {
                     break;
                 }
+                //last name
+                Console.WriteLine ("Last name:");
+                string lastName = Console.ReadLine ();
 
-                Console.WriteLine("Last name:");
-                string lastName = Console.ReadLine();
-                Console.WriteLine("Identification number:");
-                string identificationNumber = Console.ReadLine();
+                //id number
+                Console.WriteLine ("Identification number:");
+                string identificationNumber = Console.ReadLine ();
 
-                list.Add(new PersonalInformation(firstName, lastName, identificationNumber));
+                //add name to the list
+                list.Add (new PersonalInformation (firstName, lastName, identificationNumber));
 
-            }
-            Console.WriteLine();
-            foreach (PersonalInformation line in list)
-            {
-                Console.WriteLine(line.firstName + " " + line.lastName);
+            } //print
+            Console.WriteLine ();
+            foreach (PersonalInformation line in list) {
+                Console.WriteLine (line.firstName + " " + line.lastName);
             }
         }
     }
 }
-
-
-
-
--- part4/objectlist/exercise_112/Program.cs
+++ part4/objectlist/exercise_112/Program.cs
@@ -12,25 +12,31 @@ namespace exercise_112
 
     while(true)
     {
+      // name for the show
       Console.Write("Name: ");
       String name = Console.ReadLine();
+      //break on empty
       if(name == "")
       {
         break;
       } 
+      //how long is the show
       Console.Write("Duration: ");
       int duration = Convert.ToInt32(Console.ReadLine());
+      
 
+      //add the nmae of show
       list.Add(new TelevisionProgram(name, duration));
     }
+    // for empty line
     Console.WriteLine();
     Console.Write("Program's maximum duration? ");
-    int max = Convert.ToInt32(Console.ReadLine());
+    int duration_max = Convert.ToInt32(Console.ReadLine());
 
-    foreach(TelevisionProgram item in list)
+    foreach(TelevisionProgram program in list)
     {
-      if (item.duration <= max){
-        Console.WriteLine(item);
+      if (duration_max >= program.duration){
+        Console.WriteLine(program.name + ", " +program.duration + " minutes");
 
       }
     }
