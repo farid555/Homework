e07674caaaff48f94aaa651a35d12b8838a23050
farid555 <mohammad.farid@centria.fi>
2020-03-10 23:57:57 +0200

    exercise_114

--- part4/files/exercise_114/Program.cs
+++ part4/files/exercise_114/Program.cs
@@ -5,7 +5,22 @@ namespace exercise_114
     class Program
     {
         public static void Main(string[] args)
-        {
+        {    
+             //start count
+            int count = 0;    
+           
+            while (true)
+            {
+                string user_input = Console.ReadLine();
+             if (user_input == "end")
+             {
+                 break;
+             }
+                count++;
+            }
+             //print number of lines
+            Console.WriteLine(count);
+             
 
         }
     }
