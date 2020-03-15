10a3672ce59e911ae2ab1f6659bc32e5134ccb35
farid555 <mohammad.farid@centria.fi>
2020-03-15 15:16:55 +0200

    exercise_130 done

--- part5/references/exercise_126/Program.cs
+++ part5/references/exercise_126/Program.cs
@@ -7,6 +7,20 @@ namespace exercise_126
     public static void Main(string[] args)
     {
       // Try your code here, if you want
+      PaymentTerminal lunchCafeteria = new PaymentTerminal();
+
+
+      double change = lunchCafeteria.DrinkCoffee(10);
+      Console.WriteLine("remaining change "  + change);
+
+      change = lunchCafeteria.DrinkCoffee(5);
+      Console.WriteLine("remaining change "  + change);
+
+      change = lunchCafeteria.EatLunch(20);
+      Console.WriteLine("remaining change "  + change);
+
+      Console.WriteLine(lunchCafeteria);
+      
     }
   }
 }
--- part5/references/exercise_129/Program.cs
+++ part5/references/exercise_129/Program.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 
-namespace exercise_129
-{
-  class Program
-  {
-    public static void Main(string[] args)
-    {
+namespace exercise_129 {
+  class Program {
+    public static void Main(string[] args) {
       // Try your code here, if you want
-      
+      Song jackSparrow = new Song("The Lonely Island", "Jack Sparrow", 196);
+      Song anotherSparrow = new Song("The Lonely Island", "Jack Sparrow", 196);
+
+      if (jackSparrow.Equals(anotherSparrow))
+      {
+        Console.WriteLine("Songs are equal.");
+      }
+
+      if (jackSparrow.Equals("Another object")) 
+      {
+        Console.WriteLine("Strange things are afoot.");
+      }
     }
 
   }
