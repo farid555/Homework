af805b4b93aaddd028ee36c022e03addeb58af13
farid555 <mohammad.uzzaman2@centria.fi>
2020-04-12 01:40:09 +0300

    exercise_139 done

--- part8/exercise_139/src/Exercise/Abbreviations.cs
+++ part8/exercise_139/src/Exercise/Abbreviations.cs
@@ -4,20 +4,44 @@ namespace Exercise
 {
   public class Abbreviations
   {
+     //add dictionary for abbreves;
+     private Dictionary<string, string> abbRevs;
 
+     // add empty constructor
+     public Abbreviations()
+     {
+       this.abbRevs = new Dictionary<string, string>();
+     }
+
+     // add to abbrevs
     public void AddAbbreviation(string abbreviation, string explanation)
     {
-
+      this.abbRevs.Add(abbreviation, explanation);
     }
-
-    public bool HasAbbreviation(string abbreviation)
-    {
-      return false;
+        // check if abbrevs contain given string
+    public bool HasAbbreviation(string abbreviation){
+      
+      if(this.abbRevs.ContainsKey(abbreviation))
+      {
+        return true;
+      }   
+      else
+      {
+        return false;
+      }
     }
-
+    
+        //return abbrev, if none return not found
     public string FindExplanationFor(string abbreviation)
     {
-      return null;
+      if (HasAbbreviation(abbreviation))
+      {
+        return abbRevs[abbreviation];
+      }
+      else
+      {
+        return "not found";
+      }
     }
   }
 }
