7e36bc4ec899e92b33988477b1134a3c9e1cba72
farid555 <mohammad.uzzaman2@centria.fi>
2020-04-16 01:54:52 +0300

    exercise 140 done

--- part8/exercise_142/src/Exercise/IOU.cs
+++ part8/exercise_142/src/Exercise/IOU.cs
@@ -4,9 +4,16 @@ namespace Exercise
 {
   public class IOU
   {
+    private Dictionary<string, int> dictionary;
+
+    PUBLIC IOU()
+    {
+      this.dictionary = new Dictionary<string, int>();
+    }
+
     public void ChangeDebt(string toWhom, int amount)
     {
-
+      if (newAmount = this.dictionary[toWhom] + amount;)
     }
 
     public int HowMuchDoIOweTo(string toWhom)
