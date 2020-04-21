c1208c18a80a32930cfef1d42bf9a8d051f610e5
farid555 <mohammad.uzzaman2@centria.fi>
2020-04-22 01:53:00 +0300

    exercise_142 done

--- part8/exercise_142/src/Exercise/IOU.cs
+++ part8/exercise_142/src/Exercise/IOU.cs
@@ -1,24 +1,51 @@
-
+using System.Collections.Generic;
 
 namespace Exercise
 {
   public class IOU
   {
     private Dictionary<string, int> dictionary;
+    //constructor public IOU() creates a new IOU
 
-    PUBLIC IOU()
+    public IOU()
     {
       this.dictionary = new Dictionary<string, int>();
     }
-
+        // saves the amount owed and the person owed to the IOU. it can use this to also the debt, but the total amount cannot negative! 
     public void ChangeDebt(string toWhom, int amount)
     {
-      if (newAmount = this.dictionary[toWhom] + amount;)
+      if (this.dictionary.ContainsKey(toWhom))
+    {
+      int newAmount = this.dictionary[toWhom] + amount;
+      if (newAmount > 0)
+      {
+        this.dictionary[toWhom] = newAmount;
+      }
+      else
+      {
+          this.dictionary[toWhom] = 0;
+      }
     }
-
+      else if (amount > 0)
+      {
+        this.dictionary[toWhom] = amount;
+      }
+      else
+      {
+        this.dictionary[toWhom] = 0;
+      }
+    }
+     // returns the amount owed to the person whose name is given as a parameter. ifthe person cannot be found, it returns 0.
     public int HowMuchDoIOweTo(string toWhom)
     {
-      return 0;
+       if (this.dictionary.ContainsKey(toWhom))
+       {
+         return this.dictionary[toWhom];
+       }
+       else
+       {
+         return 0;
+       }
     }
-  }
+ }
 }
