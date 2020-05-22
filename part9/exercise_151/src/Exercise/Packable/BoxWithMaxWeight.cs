59c195f46a2708c95af797cc43916ecf74d369da
farid555 <mohammad.uzzaman2@centria.fi>
2020-05-23 00:51:51 +0300

    it is done

--- part9/exercise_151/src/Exercise/Packable/BoxWithMaxWeight.cs
+++ part9/exercise_151/src/Exercise/Packable/BoxWithMaxWeight.cs
@@ -3,18 +3,29 @@ namespace Exercise
   using System.Collections.Generic;
   public class BoxWithMaxWeight : Box
   {
+    private List<Iteam> list;
+    private int boxedItemsTotalWeight;
+    private int capacity;
     public BoxWithMaxWeight(int capacity)
     {
+      this.capacity = capacity;
+      this.boxedItemsTotalWeight = 0;
+      this.list = new List<Item>(); 
     }
 
     public override void Add(Item item)
     {
+      if (this.capacity - this.boxedItemsTotalWeight >= item.weight)
+      {
+        boxedItems.Add (item);
+        this.boxedItemsTotalWeight += item.weight;
+      }
     }
 
 
     public override bool IsInBox(Item item)
     {
-      return false;
+      return this.boxedItems.Contains(item);
     }
   }
 }
