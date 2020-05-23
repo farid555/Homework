1531d94238c669dbb0d3dc5806853abb48eeb27d
farid555 <mohammad.uzzaman2@centria.fi>
2020-05-23 19:48:04 +0300

    it is done

--- part9/exercise_154/src/Exercise/Herd/Organism.cs
+++ part9/exercise_154/src/Exercise/Herd/Organism.cs
@@ -1,19 +1,26 @@
 namespace Exercise
 {
+  using System.Collections.Generic;
+ 
   public class Organism : IMovable
-  {
+  {  
+    private int x;
+    private int y;
 
     public Organism(int x, int y)
     {
+      this.x = x;
+      this.y = y;
     }
     public void Move(int dx, int dy)
     {
-
+       this.x += dx;
+       this.y += dy;
     }
 
     public override string ToString()
     {
-      return "";
+      return "x: " + this.x + ";" + " y: " + this.y;
     }
   }
 }
--- part9/exercise_154/src/Exercise/Program.cs
+++ part9/exercise_154/src/Exercise/Program.cs
@@ -1,13 +1,25 @@
 using System;
 using System.Collections.Generic;
 
-namespace Exercise
-{
-  public class Program
-  {
-    public static void Main(string[] args)
-    {
+namespace Exercise {
+  public class Program {
+    public static void Main (string[] args) {
+      Organism organism = new Organism (20, 30);
+      Console.WriteLine(organism);
+      organism.Move(-10, 5);
+      Console.WriteLine(organism);
+      organism.Move(50, 20);
+      Console.WriteLine(organism);
 
+      Console.WriteLine();
+
+      Herd herd = new Herd();
+      herd.AddToHerd(new Organism(57, 66));
+      herd.AddToHerd(new Organism(73, 56));
+      herd.AddToHerd(new Organism(46, 52));
+      herd.AddToHerd(new Organism(19, 107));
+      Console.WriteLine(herd);
+     
     }
   }
 }
