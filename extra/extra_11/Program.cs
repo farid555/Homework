2f04b54c580d3e6667ae9794b168b00d218b5b4c
farid555 <mohammad.uzzaman2@centria.fi>
2020-06-19 23:07:25 +0300

    nice one

--- extra/extra_11/Program.cs
+++ extra/extra_11/Program.cs
@@ -7,7 +7,15 @@ namespace extra_11
     public static void Main(string[] args)
     {
       // Add your code here:
+      Console.WriteLine ("Give a sentence:");
+      string user_input = Console.ReadLine();
 
+      string[] splits = user_input.Split(' ');
+      
+      foreach (var word in splits)
+      {
+          System.Console.WriteLine($"{word}");
+      }
     }
   }
 }
