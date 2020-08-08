b0c6c7365a3aa93c174178f2a6d523c0d667a2db
farid555 <mohammad.uzzaman2@centria.fi>
2020-08-09 02:34:28 +0300

    extra 02 is done

--- extra/extra_02/Program.cs
+++ extra/extra_02/Program.cs
@@ -7,7 +7,25 @@ namespace extra_02
     public static void Main(string[] args)
     {
       // Add your code here:
-
+      // ask the user input integers
+     
+     Console.WriteLine("Give three number:");
+     int num_one = Convert.ToInt32(Console.ReadLine());
+     int num_two = Convert.ToInt32(Console.ReadLine());
+     int num_three = Convert.ToInt32(Console.ReadLine());
+     
+     if (num_one > num_two)
+     {
+       Console.WriteLine("Largest: " + num_one);
+     }
+     else if (num_two > num_three)
+     {
+       Console.WriteLine("Largest: " + num_two);
+     }
+     else
+     {
+       Console.WriteLine("Largest: " + num_three);
+     }
     }
   }
 }
