76c336fb019439803c8e39214e04ef2c1f3478c6
farid555 <mohammad.uzzaman2@centria.fi>
2020-08-09 01:32:24 +0300

    extra 01 is done

--- extra/extra_01/Program.cs
+++ extra/extra_01/Program.cs
@@ -7,7 +7,20 @@ namespace extra_01
     public static void Main(string[] args)
     {
       // Add your code here:test 
+      
+      Console.WriteLine("What do you want to print???");
+      string input = Console.ReadLine();
+         
+      Console.WriteLine("How many times you want to print?");
+      int num_of_times = Convert.ToInt32(Console.ReadLine());
+      int num = 0;
 
+      while(num_of_times> num)
+      {
+        Console.WriteLine(input);
+        num++;
+      }
+      
     }
   }
 }
