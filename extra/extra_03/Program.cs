1a46e7116abf9a243ae39ed4de13fc3a3457e0ff
farid555 <mohammad.uzzaman2@centria.fi>
2020-08-11 00:38:23 +0300

    extra_03 is done

--- extra/extra_03/Program.cs
+++ extra/extra_03/Program.cs
@@ -1,13 +1,42 @@
 ﻿using System;
-
+using System.Collections.Generic;
+//Ask the user for amount of integers,
+//and then that many integers. Calculate and print their sum, multiplication and average.
 namespace extra_03
 {
   class Program
   {
     public static void Main(string[] args)
-    {
-      // Add your code here:
+    { 
+       
+      Console.WriteLine("How many numbers?");
+      int howmany = Convert.ToInt32(Console.ReadLine());
 
+      int num = 0;
+
+      while(howmany > num)
+      {
+        num++;
+      }
+
+      Console.WriteLine("Give " + howmany + " numbers: ");
+      int sum = 0;
+      int multi = 1;
+      //int valid = 0;
+        
+        for(int i =0; i < howmany; i++)
+        {
+          int number = Convert.ToInt32(Console.ReadLine());
+          sum = sum + number; 
+          multi = multi * number;
+          //valid++;
+          
+        }
+            double average = ((double) sum / howmany);
+
+        Console.WriteLine("Their sum: " + sum);
+        Console.WriteLine("Their total: " + multi);
+        Console.WriteLine("Their average: " + average);
     }
   }
 }
