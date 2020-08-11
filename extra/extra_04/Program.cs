c8028e19f170b6b75e1f16d85fbae09464e8e968
farid555 <mohammad.uzzaman2@centria.fi>
2020-08-11 12:23:07 +0300

    extra_04 is done

--- extra/extra_04/Program.cs
+++ extra/extra_04/Program.cs
@@ -7,7 +7,43 @@ namespace extra_04
     public static void Main(string[] args)
     {
       // Add your code here:
+      Console.WriteLine("Give your percent [0 - 100]:");
+      int input_percent = Convert.ToInt32(Console.ReadLine());
 
-    }
+     
+          if (input_percent < 1)
+            {
+              Console.WriteLine("Impossible ");
+            }
+         else if (input_percent < 50)
+            {
+              Console.WriteLine("Grade: Fail");
+            }
+        else if (input_percent < 60)
+            {
+              Console.WriteLine("Grade: 1");
+            }
+        else if (input_percent < 70)
+            {
+              Console.WriteLine("Grade: 2");
+            }
+        else if (input_percent < 80)
+            {
+              Console.WriteLine("Grade: 3");
+            }
+        else if (input_percent < 90)
+            {
+              Console.WriteLine("Grade: 4");
+            }
+        else if (input_percent < 101)
+            {
+              Console.WriteLine("Grade: 5");
+            }
+            else
+                   {
+                    Console.WriteLine("Outstanding!");
+                   }
+      
+      }
   }
 }
