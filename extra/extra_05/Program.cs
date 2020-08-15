43feb03295c083a6fbe0b2f77a026e0ec6bc61ff
farid555 <mohammad.uzzaman2@centria.fi>
2020-08-15 22:37:01 +0300

    extra_05 is done

--- extra/extra_05/Program.cs
+++ extra/extra_05/Program.cs
@@ -1,13 +1,35 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace extra_05
 {
   class Program
   {
     public static void Main(string[] args)
     {
-      // Add your code here:
-
+       for(int i = 1; i < 6; i++)
+       {
+         Console.WriteLine(i);
+       }
+      
+        
+        Console.WriteLine("Give words, 'end' quits:");
+        List<string> list = new List<string>(); 
+        
+       while(true)
+       {
+         string input = Console.ReadLine();
+         if (input == "end")
+         {
+           break;
+         }
+         list.Add(input);
+        
+       }
+       foreach (string word in list)
+       {
+           Console.WriteLine(word);
+       }
+    
     }
   }
 }
