809540f61b1bf4186a69ca6610a992a509932fa7
farid555 <mohammad.uzzaman2@centria.fi>
2020-08-15 23:54:45 +0300

    extra_06 is done

--- extra/extra_06/Program.cs
+++ extra/extra_06/Program.cs
@@ -1,13 +1,29 @@
 ﻿using System;
+using System.Collections;
+using System.Data;
 
-namespace extra_06
-{
-  class Program
-  {
-    public static void Main(string[] args)
-    {
+namespace extra_06 {
+  class Program {
+    public static void Main (string[] args) {
       // Add your code here:
 
+      int i = 0;
+      Console.WriteLine("GIVE ME TWO INT INPUT:");
+      int input_1 = Convert.ToInt32(Console.ReadLine());
+      int input_2 = Convert.ToInt32(Console.ReadLine());
+
+      i = input_1 + input_2;
+      
+      if (i > 0 )
+      {
+         Console.WriteLine("The square root is:" + Math.Sqrt(i));
+      }
+      else
+      {
+        Console.WriteLine("printing 0");
+      }
+     
     }
   }
+
 }
